54be03bbba72a7441bda5e9abe3060df93bb3b72
timnavigate <timnavigate@gmail.com>
2021-11-26 18:07:09 +0300

    add sql's error catching
    add sq's command for catching error's code (os-level, tns/sql-level)

--- query.sql
+++ query.sql
@@ -1,12 +1,16 @@
 SET ECHO OFF
 ;
 
-/*---- executable sql query/queries ----*/
+/*---- commons sql commands ------------*/
+WHENEVER OSERROR EXIT 1
+;
+WHENEVER SQLERROR EXIT 2
+;
 
+/*---- executable sql query/queries ----*/
 SELECT * FROM DUAL
 ;
 
-/*--------------------------------------*/
 
 EXIT 0
 ;
